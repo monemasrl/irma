bce37783a067b82d0082f719c44baa78d74b2b0c
Krilliam <79572252+Krilliam@users.noreply.github.com>
2022-03-15 09:46:50 +0100

    Update arduino_communication.py

--- arduino-py-communication/arduino_communication.py
+++ arduino-py-communication/arduino_communication.py
@@ -1,23 +1,15 @@
 import serial,time,os
 if __name__ == '__main__':
     print('Running. Press CTRL-C to exit.')
-    FIFO = 'myfifo'
-    with open(FIFO) as fifo:
+    FIFO = '../myfifo'
+    try:
         with serial.Serial("/dev/ttyUSB0", 115200, timeout=1) as arduino:
-            time.sleep(0.1) #wait for serial to open
-            if arduino.isOpen():
-                try:
-                    while True:
-                    # while arduino.inWaiting()==0: pass
-                        #if  arduino.inWaiting()>0:
-                            #answer=str(arduino.readline())
-                            #print(">>>{}".format(answer))
-
-                        #while True:
-
-                        for line in fifo:
-                            arduino.write(line.encode())
-                        #time.sleep(0.5) #wait for arduino to answer
-                except KeyboardInterrupt:
-                    print("KeyboardInterrupt has been caught.")
+            with open(FIFO) as fifo:
+                time.sleep(0.1) #wait for serial to open
+                if arduino.isOpen():
+                    for line in fifo:
+                        print("Invio :{}".format(line.encode()))
+                        arduino.write(5)
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt has been caught.")
 
