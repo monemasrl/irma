58c4fc10f9a9090b5c532d686b4075097b7e4c31
Krilliam <79572252+Krilliam@users.noreply.github.com>
2022-03-15 12:15:01 +0100

    Modifiche alla gestione del canbus

--- arduino-py-communication/arduino_communication.py
+++ arduino-py-communication/arduino_communication.py
@@ -1,17 +1,16 @@
 import serial,time,os,can
 if __name__ == '__main__':
+    dati=0
     print('Running. Press CTRL-C to exit.')
-    FIFO = '../myfifo'
+    bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=125000)
     try:
-        bus = can.interface.Bus(bustype='seeedstudio', channel='can0', bitrate=125000)
-        for msg in bus:
-            print(f"{msg.arbitration_id:X}: {msg.data}")
-        #with serial.Serial("/dev/ttyUSB0", 115200, timeout=1) as arduino:
-            #with open(FIFO) as fifo:
-                #time.sleep(0.1) #wait for serial to open
-                #if arduino.isOpen():
-                    #for line in fifo:
-                        #print("Invio :{}".format(line.encode()))
-                        #arduino.write(5)
+        with serial.Serial("/dev/ttyUSB0", 115200, timeout=1) as arduino:
+            if arduino.isOpen():
+                for msg in bus:
+                    dati=int.from_bytes(msg.data, byteorder='big', signed=False)
+                    print(f"id: {msg.arbitration_id:X}")
+                    print(f"Invio i dati ricevuti : {dati}")
+                    arduino.write(dati)
     except KeyboardInterrupt:
         print("KeyboardInterrupt has been caught.")
+
