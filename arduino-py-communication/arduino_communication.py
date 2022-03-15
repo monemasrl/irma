645aadd7026d39c2c6ca02757ae63883522de5b6
Krilliam <79572252+Krilliam@users.noreply.github.com>
2022-03-15 16:44:59 +0100

    working

--- arduino-py-communication/arduino_communication.py
+++ arduino-py-communication/arduino_communication.py
@@ -4,13 +4,13 @@ if __name__ == '__main__':
     print('Running. Press CTRL-C to exit.')
     bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=125000)
     try:
-        with serial.Serial("/dev/ttyUSB0", 115200, timeout=1) as arduino:
+        with serial.Serial("/dev/ttyUSB1", 115200, timeout=1) as arduino:
             if arduino.isOpen():
                 for msg in bus:
                     dati=int.from_bytes(msg.data, byteorder='big', signed=False)
                     print(f"id: {msg.arbitration_id:X}")
                     print(f"Invio i dati ricevuti : {dati}")
-                    arduino.write(dati)
+                    arduino.write("{}".format(dati).encode('utf-8'))
     except KeyboardInterrupt:
         print("KeyboardInterrupt has been caught.")
 
