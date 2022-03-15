ab4218ab7056c399e986b061662e5bca541b4d62
Krilliam <79572252+Krilliam@users.noreply.github.com>
2022-03-15 09:24:51 +0100

    Update arduino_communication.py

--- arduino-py-communication/arduino_communication.py
+++ arduino-py-communication/arduino_communication.py
@@ -1,22 +1,23 @@
-import serial,time
+import serial,time,os
 if __name__ == '__main__':
     print('Running. Press CTRL-C to exit.')
-    with serial.Serial("/dev/ttyUSB0", 115200, timeout=1) as arduino:
-        time.sleep(0.1) #wait for serial to open
-        if arduino.isOpen():
-            print("{} connected!".format(arduino.port))
-            try:
-                while True:
-                    while arduino.inWaiting()==0: pass
-                    if  arduino.inWaiting()>0:
-                      answer=str(arduino.readline())
-                      print("{}".format(answer))
-                      with open('binary_dump.log') as f:
-                         for last_line in f:
-                           pass
-                         print(last_line[19:])
-                         arduino.write(last_line.encode())
-                    time.sleep(0.5) #wait for arduino to answer
-            except KeyboardInterrupt:
-                print("KeyboardInterrupt has been caught.")
+    FIFO = 'myfifo'
+    with open(FIFO) as fifo:
+        with serial.Serial("/dev/ttyUSB0", 115200, timeout=1) as arduino:
+            time.sleep(0.1) #wait for serial to open
+            if arduino.isOpen():
+                try:
+                    while True:
+                    # while arduino.inWaiting()==0: pass
+                        #if  arduino.inWaiting()>0:
+                            #answer=str(arduino.readline())
+                            #print(">>>{}".format(answer))
+
+                        #while True:
+
+                        for line in fifo:
+                            arduino.write(line.encode())
+                        #time.sleep(0.5) #wait for arduino to answer
+                except KeyboardInterrupt:
+                    print("KeyboardInterrupt has been caught.")
 
