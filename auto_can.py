11973f6ac91b4a3977d5feed46580ce4c492f66a
Krilliam <79572252+Krilliam@users.noreply.github.com>
2022-03-18 17:04:14 +0100

    Create auto_can.py

---
+++ auto_can.py
@@ -0,0 +1,17 @@
+
+import can,time
+if __name__ == '__main__':
+    bus = can.Bus(interface='socketcan',channel='can0')
+    try:
+        while True:
+            message = can.Message(arbitration_id=123, is_extended_id=True,data=[0x11])
+            bus.send(message, timeout=0.2)
+            print("Message 1 sent")
+            time.sleep(2)
+            message = can.Message(arbitration_id=123, is_extended_id=True,data=[0x09])
+            bus.send(message, timeout=0.2)
+            print("Message 2 sent")
+            time.sleep(2)
+
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt has been caught.")
