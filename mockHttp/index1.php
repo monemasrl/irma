f6ec30ff8e8997a9cd708acb08c1cff46ff0baa2
Krilliam <79572252+Krilliam@users.noreply.github.com>
2022-03-29 17:45:32 +0200

    Update index1.php

--- mockHttp/index1.php
+++ mockHttp/index1.php
@@ -1,10 +1,22 @@
-<html>
-<body>
 <?php
-    $data = json_decode(file_get_contents('php://input'), true);
-    var_dump($data);
-    echo $data->applicationID;
-?>
+	header("Content-Type: application/json");
+	$payload = file_get_contents('php://input');
+   $var = json_decode($payload,true);
+   $var2 = implode("|",$var);
 
-</body>
-</html>
+   $applicationID =$var['applicationID'];
+   $applicationName =$var['applicationName'];
+   $deviceName =$var['deviceName'];
+   $devEUI =$var['devEUI'];
+   $data64    = $var['data'];
+
+   foreach($var['rxInfo'] as $value)
+      {			 
+	   $gatewayID =$value['gatewayID'];
+	   $name =$value['name'];
+	   $time =$value['time'];
+	   $rssi =$value['rssi'];
+	   $loRaSNR =$value['loRaSNR'];
+    }
+    var_dump($data64);
+?>
