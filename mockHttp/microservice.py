79bbd1f8296cb7314c6eafa84ab9884411d53c6c
Krilliam <79572252+Krilliam@users.noreply.github.com>
2022-04-08 12:12:50 +0200

    Update microservice.py

--- mockHttp/microservice.py
+++ mockHttp/microservice.py
@@ -5,12 +5,10 @@ from chirpstack_api.as_pb import integration
 from google.protobuf.json_format import Parse
 
 import json
-import flask
+from flask import Flask, request, jsonify
 from flask_mongoengine import MongoEngine
 
-app = flask.Flask(__name__)
-
-
+app = Flask(__name__)
 
 app.config['MONGODB_SETTINGS'] = {
     'db': 'irma',
@@ -21,24 +19,27 @@ db = MongoEngine()
 db.init_app(app)
 
 
-newPayload={
-    "m2m:cin":{
-        "con":{
-
-        }
-    }
-}
+class Payload(db.Document):
+    metadata = db.StringField()
+    sensorData = db.StringField()
+    def to_json(self):
+        return {"metadata": self.metadata,
+                "sensorData": self.sensorData}
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Microservice</h1><p>This site is a prototype microservice for IRMA.</p>"
 
 
 @app.route('/', methods=['POST'])
 def update_record():
-    record = json.loads(request.data)
     
-
+    record = json.loads(request.data)
+    payload = Payload(metadata=record['applicationID'],
+                sensorData=record['applicationID'])
+    print(record['objectJSON'])
+    payload.save()
+    return jsonify(payload.to_json())
 
 if __name__ == "__main__":
     app.run(debug = True)
