3c57fbed3452a5c5d4d4c27cdfeb9669dd91300e
Krilliam <79572252+Krilliam@users.noreply.github.com>
2022-05-10 12:42:46 +0200

    Update downlink.py

--- downlink.py
+++ downlink.py
@@ -9,8 +9,8 @@ port = 1883
 topic = "application/5/device/2232330000888802/command/down"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
-# username = 'emqx'
-# password = 'public'
+username = 'irma'
+password = 'irma'
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
@@ -20,7 +20,7 @@ def connect_mqtt():
             print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
-    #client.username_pw_set(username, password)
+    client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
@@ -29,8 +29,8 @@ def connect_mqtt():
 def publish(client):
     msg_count = 0
     while True:
-        time.sleep(1)
-        result = client.publish(topic, "U3RvcA==")
+        time.sleep(1)  
+        result = client.publish(topic, '{"confirmed": false, "fPort": 2, "data": "U3RvcA=="}')#Stops the reading process of the end-device
         # result: [0, 1]
         status = result[0]
         if status == 0:
