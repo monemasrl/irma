34452fcbc1308339b10e83bb20669fc9ec3b7671
Krilliam <79572252+Krilliam@users.noreply.github.com>
2022-05-20 12:41:17 +0200

    Update microservice.py

--- mockHttp/microservice.py
+++ mockHttp/microservice.py
@@ -8,6 +8,8 @@ import json
 from flask import Flask, request, jsonify
 from flask_mongoengine import MongoEngine
 
+from datetime import datetime
+
 app = Flask(__name__)
 
 app.config['MONGODB_SETTINGS'] = {
@@ -62,11 +64,47 @@ class SentDocument(db.DynamicDocument):
 def home():
     payload = Payload.objects().order_by('-id').first()#restituisce l'ultimo payload nel database come oggetto di tipo Payload
     #aggiungere creazione di un json che restituisce gli stati, l'ID e i dati degli stacker nel formato fornito da paolo
+    id=payload['m2m:cin']['con']['metadata']['sensorId']
+    dato=(payload['m2m:cin']['sensorData']['objectJSON'])
+    dato=dato.replace("{\"sensorData\":","")
+    dato=dato.replace("}","")#ora la stringa contiene solo il valore numerico del campo sensorData
+    dato=int(dato)#conversione a dato numerico per confronto con soglie preimpostate
+    #print(dato)
+    if(dato<15):
+        state="ok"
+    else:
+        state="alert"
     
-    print(payload['m2m:cin']['sensorData']['objectJSON']['sensorData'])
-    
-    #toSend = SentDocument(code=payload['sensorId'],)
-    return jsonify(payload)
+    collect=Payload.objects().order_by('-id')
+    sum=0
+    sumM=0
+    count=0
+    countM=0
+    totAverage=0
+    monthlyAverage=0
+    currentMonth = datetime.now().month
+    while payload['m2m:cin']['con']['metadata']['sensorId']=='5': #da modificare perche' scorra tutti gli id
+        for x in collect:
+            app=x['m2m:cin']['sensorData']['objectJSON']
+            app=app.replace("{\"sensorData\":","")
+            app=app.replace("}","")
+            app=int(app)
+
+            appM=x['m2m:cin']['con']['metadata']['readingTimestamp']
+            appM=appM[5:7]
+            appM=int(appM)
+            if(appM==currentMonth):
+                sumM=sumM+app
+                countM=countM+1
+
+            sum=sum+app
+            count=count+1
+
+        totAverage=sum/count
+        monthlyAverage=sumM/countM
+
+    toSend = SentDocument(code=id,status=state,titolo1="Media Letture Totali",dato1=totAverage,titolo2="Media Letture Mensili",dato2=monthlyAverage,titolo3="app",dato3="app")
+    return jsonify(toSend.to_jsonSent())
 
 
 @app.route('/', methods=['POST'])
@@ -76,8 +114,8 @@ def create_record():
                 time=record['publishedAt'],
                 latitude=record['rxInfo'][0]['location']['latitude'],
                 longitude=record['rxInfo'][0]['location']['longitude'],
-                sensorData=record)
-    #print(record['objectJSON'])
+                sensorData=record
+                )
     data = payload.from_json(json.dumps(payload.to_json()))
     data.save()
     return jsonify(payload.to_json())
