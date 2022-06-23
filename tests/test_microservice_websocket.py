3f471e6d506e711b908995a913f28a78def924e4
TheRealLorenz <lollobellina@gmail.com>
2022-06-23 17:56:35 +0200

    Testing of db objects moved to 'test_microservice_db.py'

--- tests/test_microservice_websocket.py
+++ tests/test_microservice_websocket.py
@@ -2,62 +2,6 @@ from mockHttp import microservice_websocket
 import pytest
 import json
 from fixtures.data_fixtures import *
-from fixtures.db_fixtures import *
-
-
-class TestDBDocs:
-
-    def test_Payload_to_json(self, payload):
-        payload_dict = {
-            "m2m:cin": {
-                "con": {
-                    "metadata": {
-                        "sensorId": payload.iD,
-                        "readingTimestamp": payload.time,
-                        "latitude": payload.latitude,
-                        "longitude": payload.longitude,
-                    }
-                },
-                "sensorData": payload.sensorData,
-            }
-        }
-
-        assert payload.to_json() == payload_dict, "Error in `Payload.to_json`: output mismatch"
-
-    def test_SentDocument_to_jsonSent(self):
-        sentDocument = microservice_websocket.SentDocument(
-            eui=123,
-            status="rec",
-            code=456,
-            titolo1="Titolo 1",
-            dato1=7.89,
-            titolo2="",
-            dato2=-2,
-            titolo3="Titolo 3!",
-            dato3=-3j
-        )
-
-        sentDocument_dict = {
-            "devEUI": 123,
-            "state": "rec",
-            "code": 456,
-            "datiInterni": [
-                {
-                    "titolo": "Titolo 1",
-                    "dato": 7.89
-                },
-                {
-                    "titolo": "",
-                    "dato": -2
-                },
-                {
-                    "titolo": "Titolo 3!",
-                    "dato": -3j
-                }
-            ]
-        }
-
-        assert sentDocument.to_jsonSent() == sentDocument_dict, "Error in `SentDocument.to_jsonSent()`: output mismatch"
 
 
 class TestFlaskApp:
