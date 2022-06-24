65b695011ca65b87cd5fcb4953fe5f337087f575
TheRealLorenz <lollobellina@gmail.com>
2022-06-24 10:49:06 +0200

    Override 'from_json()' method in Payload class

--- mockHttp/microservice_db.py
+++ mockHttp/microservice_db.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
 from flask_mongoengine import MongoEngine, DynamicDocument
 from flask import Flask
+import json
 
 
 def init_db(app: Flask) -> MongoEngine:
@@ -24,6 +26,18 @@ class Payload(DynamicDocument):
                     "sensorData":self.sensorData
                     }
                 }
+
+    @classmethod
+    def from_json(self, s: str) -> Payload:
+        s_dict = json.loads(s)
+        new_p = Payload()
+        new_p.iD = s_dict["m2m:cin"]["con"]["metadata"]["sensorId"]
+        new_p.time = s_dict["m2m:cin"]["con"]["metadata"]["readingTimestamp"]
+        new_p.latitude = s_dict["m2m:cin"]["con"]["metadata"]["latitude"]
+        new_p.longitude = s_dict["m2m:cin"]["con"]["metadata"]["longitude"]
+        new_p.sensorData = s_dict["m2m:cin"]["sensorData"]
+
+        return new_p
 #############################################################################
 #####definizione struttura del documento da reinviare alla richiesta GET#####
 #############################################################################
