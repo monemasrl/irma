caf619a077742cdef9741649f7585e30441124e4
TheRealLorenz <lollobellina@gmail.com>
2022-06-24 17:01:38 +0200

    Add template file for testing 'downlink_microservice.py' (WIP)

---
+++ tests/test_downlink_microservice.py
@@ -0,0 +1,26 @@
+from mockHttp import downlink_microservice
+from flask import Flask
+from flask.testing import FlaskClient
+import pytest
+
+class TestFlaskApp:
+
+    @pytest.fixture()
+    def app(self) -> Flask:
+        app = downlink_microservice.create_app()
+        app.config.update({
+            "TESTING": True,
+        })
+        # set up
+        yield app
+        # clean up
+    
+    @pytest.fixture()
+    def client(self, app: Flask) -> FlaskClient:
+        return app.test_client()
+
+    # def test_main_route_get(self, client):
+    #     response = client.get("/")
+    #     print(response.data)
+    #     assert False
+    # TODO: implement testing for mqtt lib
