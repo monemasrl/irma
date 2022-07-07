aa03de0a928184617b043e4778f2920b19131a32
TheRealLorenz <lollobellina@gmail.com>
2022-07-07 12:38:21 +0200

    Edit chirpstack-docker config files

--- chirpstack-docker/init-postgresql-db.sh
+++ chirpstack-docker/init-postgresql-db.sh
@@ -3,10 +3,10 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 
-    CREATE ROLE chirpstack_ns WITH login password 'dbpassword';
+    CREATE ROLE chirpstack_ns WITH login password 'chirpstack_ns';
     CREATE DATABASE chirpstack_ns WITH owner chirpstack_ns;
 
-    CREATE ROLE chirpstack_as WITH login password 'dbpassword';
+    CREATE ROLE chirpstack_as WITH login password 'chirpstack_as';
     CREATE DATABASE chirpstack_as WITH owner chirpstack_as;
 
     \c chirpstack_as
