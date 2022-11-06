9eaa5db27b635dab858142b07cde761d5eb2c25f
TheRealLorenz <lollobellina@gmail.com>
2022-11-06 22:20:05 +0100

    Fix APScheduler

--- microservice_websocket/app/services/scheduler/__init__.py
+++ microservice_websocket/app/services/scheduler/__init__.py
@@ -8,14 +8,24 @@ from ...utils.sync_cache import sync_cached
 def init_scheduler():
     scheduler = AsyncIOScheduler()
 
-    @scheduler.task(
-        "interval", id="update_state", seconds=config["NODE_TIMEOUT_CHECK_INTERVAL"]
-    )
     def periodically_get_route():
         requests.get("http://localhost:5000/api/check")
 
-    @scheduler.task("interval", id="sync_cache", seconds=config["CHECK_SYNC_READY"])
+    scheduler.add_job(
+        periodically_get_route,
+        "interval",
+        id="update_state",
+        seconds=config["NODE_TIMEOUT_CHECK_INTERVAL"],
+    )
+
     async def periodically_sync_cache():
         await sync_cached()
+
+    scheduler.add_job(
+        periodically_sync_cache,
+        "interval",
+        id="sync_cache",
+        seconds=config["CHECK_SYNC_READY"],
+    )
 
     scheduler.start()
