e0d7699e61d6345b6f8e796b13bb7af614b97c83
TheRealLorenz <lollobellina@gmail.com>
2022-11-06 22:21:08 +0100

    Refactor orgs test

--- tests/test_microservice_websocket/test_routes/test_route_organizations.py
+++ tests/test_microservice_websocket/test_routes/test_route_organizations.py
@@ -1,18 +1,16 @@
-from flask.testing import FlaskClient
+import pytest
+from fastapi.testclient import TestClient
 
 from microservice_websocket.app.services import database as db
 
 
 class TestPostOrganizations:
-    endpoint = "/api/organizations/"
+    endpoint = "/api/organizations/create"
     name = "orgName"
 
-    @classmethod
-    def teardown_class(cls):
-        db.Organization.drop_collection()
-
     # Creating Organization
-    def test_create(self, app_client: FlaskClient, auth_header):
+    @pytest.mark.asyncio
+    async def test_create(self, app_client: TestClient, auth_header):
         response = app_client.post(
             self.endpoint,
             json={"name": self.name},
@@ -24,15 +22,19 @@ class TestPostOrganizations:
         ), "Invalid response code when posting valid data"
 
         assert (
-            len(db.Organization.objects()) == 1
+            len(await db.Organization.find_all().to_list()) == 1
         ), "Create organization doesn't persist in the database"
 
         assert (
-            db.Organization.objects().first()["organizationName"] == self.name
-        ), "Invalid organization name"
+            org := await db.Organization.find_one()
+        ) and org.organizationName == self.name, "Invalid organization name"
 
     # Creating Organization with already used name
-    def test_create_invalid_name(self, app_client: FlaskClient, auth_header):
+    @pytest.mark.asyncio
+    async def test_create_invalid_name(self, app_client: TestClient, auth_header):
+        o = db.Organization(organizationName=self.name)
+        await o.save()
+        # Done setup
 
         response = app_client.post(
             self.endpoint, json={"name": self.name}, headers=auth_header
@@ -43,11 +45,11 @@ class TestPostOrganizations:
         ), "Invalid response code when posting data with name already in use"
 
     # Creating organization with invalid payload
-    def test_create_invalid_payload(self, app_client: FlaskClient, auth_header):
+    def test_create_invalid_payload(self, app_client: TestClient, auth_header):
         response = app_client.post(self.endpoint, json={}, headers=auth_header)
 
         assert (
-            response.status_code == 400
+            response.status_code == 422
         ), "Invalid response code when posting an invalid payload when creating an organization"
 
 
@@ -55,22 +57,11 @@ class TestGetOrganizations:
     endpoint = "/api/organizations/"
     name = "orgName"
 
-    @classmethod
-    def teardown_class(cls):
-        db.Organization.drop_collection()
-
-    # Getting empty Organizations
-    def test_get_empty(self, app_client: FlaskClient, auth_header):
-        response = app_client.get(self.endpoint, headers=auth_header)
-
-        assert (
-            response.status_code == 404
-        ), "Invalid response code when getting route with empty organizations"
-
-    def test_get(self, app_client: FlaskClient, auth_header):
-        # Manual create Organization
+    @pytest.mark.asyncio
+    async def test_get(self, app_client: TestClient, auth_header):
         o = db.Organization(organizationName=self.name)
-        o.save()
+        await o.save()
+        # Done setup
 
         response = app_client.get(self.endpoint, headers=auth_header)
 
@@ -79,9 +70,9 @@ class TestGetOrganizations:
         ), "Invalid response code when getting route with organizations present"
 
         assert (
-            len(response.json["organizations"]) == 1
+            len(response.json()["organizations"]) == 1
         ), "Invalid payload lenght when querying organizations"
 
         assert (
-            response.json["organizations"][0]["organizationName"] == self.name
+            response.json()["organizations"][0]["organizationName"] == self.name
         ), "Invalid Organization name"
