cfab11ab1c762738078c4a64c72baee33eacc334
TheRealLorenz <lollobellina@gmail.com>
2022-11-25 16:13:13 +0100

    Embed node check in scheduler function

--- microservice_websocket/app/blueprints/api/__init__.py
+++ microservice_websocket/app/blueprints/api/__init__.py
@@ -1,7 +1,5 @@
 from fastapi import APIRouter
 
-from ...services.database import Node
-from ...utils.node import update_state
 from .alert import alert_router
 from .application import application_router
 from .command import command_router
@@ -25,27 +23,3 @@ main_router.include_router(organization_router)
 main_router.include_router(payload_routr)
 main_router.include_router(session_router)
 main_router.include_router(user_router)
-
-
-@main_router.get("/check")
-async def check_for_updates():
-    from ... import socketManager
-
-    nodes: list[Node] = await Node.find_all().to_list()
-
-    update_frontend = False
-
-    for node in nodes:
-        new_state = update_state(node.state, node.lastSeenAt)
-
-        if node.state != new_state:
-            update_frontend = True
-            node.state = new_state
-            await node.save()
-
-    if update_frontend:
-        print("Detected node-state change(s), emitting 'change'")
-        await socketManager.emit("change-reading")
-        await socketManager.emit("change")
-
-    return {"message": ("Changed" if update_frontend else "Not Changed")}
--- microservice_websocket/app/services/scheduler/__init__.py
+++ microservice_websocket/app/services/scheduler/__init__.py
@@ -1,18 +1,36 @@
-import requests
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from ...config import config as Config
+from ...services.database import Node
+from ...utils.node import update_state
 from ...utils.sync_cache import sync_cached
 
 
 def init_scheduler():
     scheduler = AsyncIOScheduler()
 
-    def periodically_get_route():
-        requests.get("http://localhost:8000/api/check")
+    async def periodically_check_nodes():
+        from ... import socketManager
+
+        nodes: list[Node] = await Node.find_all().to_list()
+
+        update_frontend = False
+
+        for node in nodes:
+            new_state = update_state(node.state, node.lastSeenAt)
+
+            if node.state != new_state:
+                update_frontend = True
+                node.state = new_state
+                await node.save()
+
+        if update_frontend:
+            print("Detected node-state change(s), emitting 'change'")
+            await socketManager.emit("change-reading")
+            await socketManager.emit("change")
 
     scheduler.add_job(
-        periodically_get_route,
+        periodically_check_nodes,
         "interval",
         id="update_state",
         seconds=Config.app.NODE_TIMEOUT_CHECK_INTERVAL,
