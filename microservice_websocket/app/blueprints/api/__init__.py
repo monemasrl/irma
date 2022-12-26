e0598babe73db60152accbc983065a294ac3758b
TheRealLorenz <lollobellina@gmail.com>
2022-12-26 13:12:16 +0100

    Removed sync_cache and ext_arch stuff

--- microservice_websocket/app/blueprints/api/__init__.py
+++ microservice_websocket/app/blueprints/api/__init__.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter
 from .alert import alert_router
 from .application import application_router
 from .command import command_router
-from .external_archiviation import ext_arch_router
 from .jwt import jwt_router
 from .node import node_router
 from .organization import organization_router
@@ -15,7 +14,6 @@ main_router = APIRouter(prefix="/api")
 main_router.include_router(alert_router)
 main_router.include_router(application_router)
 main_router.include_router(command_router)
-main_router.include_router(ext_arch_router)
 main_router.include_router(jwt_router)
 main_router.include_router(node_router)
 main_router.include_router(organization_router)
