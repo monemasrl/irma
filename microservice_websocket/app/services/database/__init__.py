f864de0ebf26b7bcc26ace2a5d60572d62e821ef
TheRealLorenz <lollobellina@gmail.com>
2022-12-29 10:07:58 +0100

    Removed leftover comments

--- microservice_websocket/app/services/database/__init__.py
+++ microservice_websocket/app/services/database/__init__.py
@@ -1,18 +1,3 @@
-# class Node(Document):
-#     def to_dashboard(self) -> dict:
-#         unhandledAlerts = Alert.objects(node=self, isHandled=False)
-#
-#         unhandledAlertIDs = [str(x["id"]) for x in unhandledAlerts]
-#
-#         return {
-#             "nodeID": self.nodeID,
-#             "nodeName": self.nodeName,
-#             "applicationID": str(self.application["id"]),
-#             "state": NodeState.to_irma_ui_state(self.state),
-#             "unhandledAlertIDs": unhandledAlertIDs,
-#         }
-
-
 from beanie import init_beanie
 from mongomock_motor import AsyncMongoMockClient
 from motor.motor_asyncio import AsyncIOMotorClient
